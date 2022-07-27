fd0505494cca0019e80391e7e52731f41166afcd
Patrick <pcoffey89@gmail.com>
2022-07-27 16:32:57 +0100

    some comments added for future plans

--- simple_tools/python/finder.py
+++ simple_tools/python/finder.py
@@ -1,20 +1,21 @@
 import os
 string1 = input("Please enter string to search for in files: ")
-dir = input("Please enter directory to search \(in linux notation\): ")
+dir = input("Please enter directory to search (in linux notation): ")
 found = []
 with os.scandir(dir) as entries:
     for entry in entries:
+        #print(entry)
         try:    
 
 #add different file types beyond natively openable types
 #this can only manage plain text files atm, annoying, but to handle more would need to import relevant modules, appropriates parse content and search, i.e. excel files - loop through sheets, then rows etc etc
 
-            state = 0
-            with open(entry, 'r') as f:
+            state = 0#let's change this to a count, also considering first and last occurence feature
+            with open(entry, 'r') as f:#next we'd need to handle file types
                 for line in f:
                     if string1 in line:
-                        state = 1        
-            if state == 1:
+                        state = 1        #count += 1
+            if state == 1:#if count > 0
                 found.append(entry)
                 
         except:
